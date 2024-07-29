485f25db7868e46d31dd0deb1de14d0c66fe2420
blake <yx.wu.work@gmail.com>
2024-07-29 20:09:43 +0800

    refactor: Use fn to replace simple main structure.

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,25 @@ fn main() {
     // input from args
     let args: Vec<String> = env::args().collect(); 
     
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = parse_configs(&args);
 
-    println!("Searcing for {query} in {file_path}");
+    println!("Searcing for {} in {}", config.query, config.file_path);
 
-    let contents = fs::read_to_string(file_path)
+    let contents = fs::read_to_string(config.file_path)
         .expect("Should have been able to read the file");
 
     println!("with text: \n{contents}");
+}
+
+struct Config{
+    query: String,
+    file_path: String,
+}
+
+
+fn parse_configs(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config{ query, file_path }
 }
