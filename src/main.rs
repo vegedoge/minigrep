49dd845772c730145f60d7a678de216c1e0685b2
blake <yx.wu.work@gmail.com>
2024-07-29 19:57:09 +0800

    Feat: Basic file reading function implemented.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::{env, fs};
 fn main() {
-    println!("Hello, world!");
+    // input from args
+    let args: Vec<String> = env::args().collect(); 
+    
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searcing for {query} in {file_path}");
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    println!("with text: \n{contents}");
 }
