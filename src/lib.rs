df466f8c60add06890482f6b6f7b7e69ec3f552a
blake <yx.wu.work@gmail.com>
2024-07-30 11:25:28 +0800

    Feat: Add case_insensitive env variable.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use std::error::Error;
-use std::fs;
+use std::{env, fs};
 
 #[cfg(test)]
 mod tests {
@@ -8,19 +8,39 @@ mod tests {
     #[test]
     fn one_result() {
         let query = "duct";
-        let contents = "
+        let contents = 
+        "
         \nRust:
         \nsafe, fast, productive.
-        \nPick three.";
+        \nPick three.
+        ";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUSt";
+        let contents = 
+        "
+        \nRust:
+        \nsafe, fast, productive.
+        \nTrust me.
+        ";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
+
 
 }
 
 pub struct Config{
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 // Construct func for Config
@@ -32,15 +52,24 @@ impl Config{
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
+        
+        // if has a value -> true else -> false
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-        Ok(Config{ query, file_path })
+        Ok(Config{ query, file_path, ignore_case })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
     let contents = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{line}")
     }
 
@@ -61,5 +90,26 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
         println!("No target found");
     }
     
+    results
+}
+
+pub fn search_case_insensitive<'a>(
+    query: &str,
+    contents: &'a str)
+-> Vec<&'a str> {
+    // turn to lower case
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    if results.len() == 0 {
+        println!("No target found");
+    }
+
     results
 }
